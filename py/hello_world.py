4945c7fb332f9e15d83e232255812301a18f4517
wi1s0n1118 <961290465@qq.com>
2019-01-16 00:29:35 +0800

    add a folder for python scripts

---
+++ py/hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+# **********************************************************
+# * Author        : Wi1s0n030
+# * Email         : 961290465@qq.com
+# * Create time   : 2019-01-16 00:27
+# * Last modified : 2019-01-16 00:27
+# * Filename      : hello_world.py
+# * Description   : echo hello world
+# **********************************************************
+
+print ("hello world!")
