befc4c87b76da51fc0c7db58de74d18ad46529cf
wi1s0n1118 <961290465@qq.com>
2019-01-16 00:06:53 +0800

    arithmatic progression

---
+++ sh/arithmatic_progression.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# **********************************************************
+# * Author        : Wi1s0n030
+# * Email         : 961290465@qq.com
+# * Create time   : 2019-01-15 23:49
+# * Last modified : 2019-01-15 23:49
+# * Filename      : arithmatic_progression.sh
+# * Description   : 
+# **********************************************************
+
+read -t 30 -p "Please input your first number:" num1
+read -t 30 -p "Please input your second number:" num2
+
+if [ $num1 -ge $num2 ]
+then
+    echo "Error! Go fuck yourself asshole!"
+    exit 1
+else
+    sum=0
+fi
+
+for ((i=$num1;i<=$num2;i=i+1))
+do
+    sum=$(($sum+$i))
+done
+
+echo "the sum of the arithmatic progression is : $sum"
+
